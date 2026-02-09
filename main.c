83ffb41ba84fc36df127ff4cda0a5105c72c60e9
waveper <waveperudomsri@gmail.com>
2026-02-09 20:38:31 +0700

    fix and things

--- main.c
+++ main.c
@@ -11,6 +11,16 @@ void usage(void) {
 }
 
 char *get_btf_data(const char *input) {
+  FILE *file = fopen(input, "rb");
+
+  unsigned dat_len = strlen(file) - 7; // 7 bytes of ignore metadata, focus on pixel data
+  char *output = malloc((dat_len * 8) + 1); // + 1 for null-terminator, also multiply with 8 because each pixel is 1-bit by design and we going to convert data in the file into string of binary
+
+  int i = 0 + 7; // maybe a joke here
+  for (i < strlen(file); i++) {
+    // uhh, how to convert each bit into output char
+  }
+
   return NULL; // TODO: get file metadata and pixel data
 }
 
@@ -39,7 +49,11 @@ int main(int argc, char *argv[]) {
       printf("Error: please input the image file\n");
       return 2;
     }
-    get_btf_data(argv[2]); // when there's no option
+    image_data = get_btf_data(argv[2]); // when there's no option
+    if (image_data = NULL) {
+      printf("Error: null returned....\n");
+      return 3;
+    }
   }
 
   return 0;
