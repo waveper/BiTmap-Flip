c2fd18a42dfc01aa7fea7ecf53e86c490ee09729
waveper <waveperudomsri@gmail.com>
2026-02-10 14:11:39 +0700

    stuff......

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 void usage(void) {
   printf("Usage: btf [option] [file.btf]\n");
@@ -11,7 +12,8 @@ void usage(void) {
   printf("made by github.com/waveper\n");
 }
 
-char *get_btf_data(const char *input) {
+char *get_btf_data(const char *input) { // get all data from binary (metadata + pixel data), it will be extractee later
+  // also verify the file
   FILE *file = fopen(input, "rb");
   if (file == NULL) {
     return NULL;
@@ -31,14 +33,53 @@ char *get_btf_data(const char *input) {
     return NULL;
   }
 
-  unsigned dat_len = (unsigned)file_size - 7; // 7 bytes of ignore metadata, focus on pixel data
+  unsigned dat_len = (unsigned)file_size;
   unsigned char *output = malloc((dat_len * 8) + 1); // + 1 for null-terminator, also multiply with 8 because each pixel is 1-bit by design and we going to convert data in the file into string of binary
 
   for (unsigned i = 7; i < (unsigned)file_size; i++) {
-    // uhh, how to convert each bit into output char
+    snprintf(output, 8, "\X20", file[i]); // TODO: actual functioning read file to binary string
   }
+  
+  // output as strings of binaries
+  return output;
+}
 
-  return NULL; // TODO: get file metadata and pixel data
+char *extract_btf_pdata(const char *input, int pixels) { // extract pixel data from string of binary and output as extracted pixel data (binary string)
+  unsigned dat_len = sizeof(input) - (7 * 8); // ignore 7 bytes of metadata, focus on pixel data
+  unsigned char *output = malloc(dat_len + 1);
+
+  for (int i = 7 * 8; i < (pixels + (7 * 8)); i++) { // pixels varuable is the numbers of pixels calculated from (width * height)
+    output[i] = input[i];
+  }
+  output[i + 1] = '\0';
+  
+  return output;
+}
+
+int extract_btf_width(const char *input) {
+  uint16_t output; // 16-bit output
+  char *bin[(2 * 8) + 1]; // binary buffer
+
+  for (int i = 4 * 8; i < (4 * 8) + (2 * 8); i++) {
+    bin[i / (4 * 8)] = input[i];
+  }
+  bin[17] = '\0';
+  output  = strtoll(bin, NULL, 2);
+
+  return output;
+}
+
+int extract_btf_height(const char *input) {
+  uint16_t output; // 16-bit output
+  char *bin[(2 * 8) + 1]; // binary buffer
+
+  for (int i = 6 * 8; i < (6 * 8) + (2 * 8); i++) {
+    bin[i / (6 * 8)] = input[i];
+  }
+  bin[17] = '\0';
+  output  = strtoll(bin, NULL, 2);
+
+  return output;
 }
 
 int main(int argc, char *argv[]) {
